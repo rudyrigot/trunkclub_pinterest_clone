8a51e09d80d9977f73211486178da1f92039c0ba
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-07 19:44:24 -0700

    Restrict user scaffold to admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def admin_only!
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: 'You were redirected from an admin-only page.' if !user_signed_in? || !current_user.admin? }
+      format.json { render status: :forbidden if !user_signed_in? || !current_user.admin? }
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,6 @@ class UsersControllerTest < ActionController::TestCase
 
   setup do
     @user = users(:user1)
-    sign_in_as :admin
   end
 
   teardown do
@@ -12,17 +11,32 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
+    sign_in_as :admin
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
+  test "should not get index if not admin" do
+    sign_in_as :user1
+    get :index
+    assert_redirected_to root_path
+  end
+
   test "should get new" do
+    sign_in_as :admin
     get :new
     assert_response :success
   end
 
+  test "should not get new if not admin" do
+    sign_in_as :user1
+    get :new
+    assert_redirected_to root_path
+  end
+
   test "should create user" do
+    sign_in_as :admin
     assert_difference('User.count') do
       post :create, user: { admin: @user.admin, email: 'foo@mail.com', first_name: @user.first_name, last_name: @user.last_name, password: 'FooBar' }
     end
@@ -30,26 +44,65 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to user_path(assigns(:user))
   end
 
+  test "should not create user if not admin" do
+    sign_in_as :user1
+    assert_difference('User.count', 0) do
+      post :create, user: { admin: @user.admin, email: 'foo@mail.com', first_name: @user.first_name, last_name: @user.last_name, password: 'FooBar' }
+    end
+
+    assert_redirected_to root_path
+  end
+
   test "should show user" do
+    sign_in_as :admin
     get :show, id: @user
     assert_response :success
   end
 
+  test "should not show user if not admin" do
+    sign_in_as :user1
+    get :show, id: @user
+    assert_redirected_to root_path
+  end
+
   test "should get edit" do
+    sign_in_as :admin
     get :edit, id: @user
     assert_response :success
   end
 
+  test "should not get edit if not admin" do
+    sign_in_as :user1
+    get :edit, id: @user
+    assert_redirected_to root_path
+  end
+
   test "should update user" do
+    sign_in_as :admin
     patch :update, id: @user, user: { admin: @user.admin, email: @user.email, first_name: @user.first_name, last_name: @user.last_name }
     assert_redirected_to user_path(assigns(:user))
   end
 
+  test "should not update user if not admin" do
+    sign_in_as :user1
+    patch :update, id: @user, user: { admin: @user.admin, email: @user.email, first_name: @user.first_name, last_name: @user.last_name }
+    assert_redirected_to root_path
+  end
+
   test "should destroy user" do
+    sign_in_as :admin
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
 
     assert_redirected_to users_path
+  end
+
+  test "should not destroy user if not admin" do
+    sign_in_as :user1
+    assert_difference('User.count', 0) do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_path
   end
 end
