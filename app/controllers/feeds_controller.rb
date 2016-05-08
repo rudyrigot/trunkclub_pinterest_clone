583763c2774648ccb148857559ca9c93163c5621
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-07 19:24:42 -0700

    Homepage

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,6 @@
 class FeedsController < ApplicationController
   def home
+    @most_recent_pins = Pin.includes(board: :user).order(created_at: :desc).limit(30)
+    @random_boards = Board.includes(:user).order("RANDOM()").limit(10)
   end
 end
