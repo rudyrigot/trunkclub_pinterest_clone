3e32e2c253b7298591e26246ea06f80a68b5494e
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-07 18:27:55 -0700

    No auth for homepage

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class FeedsControllerTest < ActionController::TestCase
 
-  setup do
-    sign_in_as :admin
-  end
-
-  teardown do
-    sign_out_if_logged_in
-  end
-
   test "should get home" do
     get :home
     assert_response :success
